5b6ace5c4a5a6ba07b44b362881bd979a55dbdf1
Andrew Leonard <anleonar@redhat.com>
2021-10-08 12:59:50 +0100

    bootjdk download using curl
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

--- build-farm/platform-specific-configurations/linux.sh
+++ build-farm/platform-specific-configurations/linux.sh
@@ -133,13 +133,12 @@ if [ ! -d "$(eval echo "\$$BOOT_JDK_VARIABLE")" ]; then
         apiUrlTemplate="https://api.\${vendor}.net/v3/binary/latest/\${JDK_BOOT_VERSION}/\${releaseType}/linux/\${ARCHITECTURE}/jdk/\${VARIANT}/normal/\${vendor}"
       fi
       apiURL=$(eval echo ${apiUrlTemplate})
-      echo "Downloading GA release of boot JDK version ${JDK_BOOT_VERSION} from https://ci.adoptopenjdk.net/job/build-scripts/job/jobs/job/jdk/job/jdk-linux-ppc64le-hotspot/508/artifact/workspace/target/OpenJDK-jdk_ppc64le_linux_hotspot_2021-10-06-18-04.tar.gz"
+      echo "Downloading GA release of boot JDK version ${JDK_BOOT_VERSION} from "https://ci.adoptopenjdk.net/view/work-in-progress/job/andrew-jdk-linux-x64-hotspot/14/artifact/workspace/target/OpenJDK-jdk_x64_linux_hotspot_2021-10-08-09-51.tar.gz
       # make-adopt-build-farm.sh has 'set -e'. We need to disable that for
       # the fallback mechanism, as downloading of the GA binary might fail.
       set +e
-      curl -L "https://ci.adoptopenjdk.net/job/build-scripts/job/jobs/job/jdk/job/jdk-linux-pffpc64le-hotspot/508/artifact/workspace/target/OpenJDK-jdk_ppc64le_linux_hotspot_2021-10-06-18-04.tar.gz" | tar xpzf - --strip-components=1 -C "$bootDir"
+      curl -L ""https://ci.adoptopenjdk.net/view/work-in-progress/job/andrew-jdk-linux-x64-hotspot/14/artifact/workspace/target/OpenJDK-jdk_x64_linux_hotspot_2021-10-08-09-51.tar.gz | tar xpzf - --strip-components=1 -C "$bootDir"
       retVal=$?
-    retVal=1
       set -e
       if [ $retVal -ne 0 ]; then
         # We must be a JDK HEAD build for which no boot JDK exists other than
