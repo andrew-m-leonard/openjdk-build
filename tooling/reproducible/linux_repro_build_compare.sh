291290c23e6622148d9b679546ce8d7e7d3de07c
Andrew Leonard <andrew.m.leonard@ibm.com>
2026-03-10 14:42:50 +0000

    debug
    Signed-off-by: Andrew Leonard <andrew.m.leonard@ibm.com>

--- tooling/reproducible/linux_repro_build_compare.sh
+++ tooling/reproducible/linux_repro_build_compare.sh
@@ -571,7 +571,7 @@ cat "$BUILD_DIR/$BUILD_FOLDER"/build/*/spec.gmk
   cat "$BUILD_DIR/$BUILD_FOLDER/repro_configure.log"
 
   echo "Executing: make images"
-  if ! echo "cd $BUILD_DIR/$BUILD_FOLDER/build/* && make images LOG=cmdlines > ../../repro_build.log 2>&1" | sh; then
+  if ! echo "cd $BUILD_DIR/$BUILD_FOLDER/build/* && make images > ../../repro_build.log 2>&1" | sh; then
     cat "$BUILD_DIR/$BUILD_FOLDER/repro_build.log" || true
     echo "OpenJDK make images failure, exiting"
     if [[ -n "$PATH_SAVE" ]]; then
